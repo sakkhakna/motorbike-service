afec2c4fd66784417d17a8e49bf17e34106825af
Sak Khakna <sthissakkhakna@gmail.com>
2024-11-13 16:35:31 +0700

    login api

--- app/(app)/customers/page.jsx
+++ app/(app)/customers/page.jsx
@@ -1,5 +1,70 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import {deleteDialog} from "@/components/ui/delete-dialog"
+
+const customers = [
+    {
+        id: 1,
+        name: "John Doe",
+        phoneNumber: "0123456789",
+        motorbikeMake: "Vespa",
+        motorbikeModel: "Sprint 150",
+        motorbikeYear: "2020",
+        motorbikeColor: "White",
+        motorbikePlate: "PP 2AB-1234",
+        motorbikeVIN: "1234567890",
+    }
+    ]
+
 export default function Page() {
     return <>
         <h1 className="text-xl font-bold">Customers</h1>
+        <div className="w-full">
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Name</TableHead>
+                        <TableHead>Phone Number</TableHead>
+                        <TableHead>Motorbike Make</TableHead>
+                        <TableHead>Motorbike Model</TableHead>
+                        <TableHead>Motorbike Color</TableHead>
+                        <TableHead>Motorbike Year</TableHead>
+                        <TableHead>Motorbike Plate</TableHead>
+                        <TableHead>Motorbike VIN</TableHead>
+                        <TableHead className="text-right">Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {customers.map((customer) => (
+                        <TableRow key={customer.id}>
+                            <TableCell className="font-medium">{customer.name}</TableCell>
+                            <TableCell>{customer.phoneNumber}</TableCell>
+                            <TableCell>{customer.motorbikeMake}</TableCell>
+                            <TableCell>{customer.motorbikeModel}</TableCell>
+                            <TableCell>{customer.motorbikeColor}</TableCell>
+                            <TableCell>{customer.motorbikeYear}</TableCell>
+                            <TableCell>{customer.motorbikePlate}</TableCell>
+                            <TableCell className="text-right">{customer.motorbikeVIN}</TableCell>
+                            <deleteDialog/>
+                        </TableRow>
+                    ))}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell colSpan={3}>Total</TableCell>
+                        <TableCell className="text-right">$2,500.00</TableCell>
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </div>
     </>
 }
--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,5 +1,9 @@
-import Link from "next/link"
+'use client'
 
+import Link from "next/link"
+import {useEffect, useState} from "react";
+import {useAuth} from "@/hooks/auth";
+import {useRouter} from "next/navigation";
 import { Button } from "@/components/ui/button"
 import {
     Card,
@@ -11,49 +15,86 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
+
 export default function Page() {
+    const router = useRouter()
+
+    const { login } = useAuth({
+        middleware: "guest",
+        redirectIfAuthenticated: "/",
+    })
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [shouldRemember, setShouldRemember] = useState(false)
+    const [errors, setErrors] = useState([])
+    const [status, setStatus] = useState(null)
+
+    useEffect(() => {
+        if (router.reset?.length > 0 && errors.length === 0) {
+            setStatus(atob(router.reset))
+        } else {
+            setStatus(null)
+        }
+    });
+
+    const submitForm = async (event) => {
+        event.preventDefault()
+
+        login({
+            email,
+            password,
+            remember: shouldRemember,
+            setErrors,
+            setStatus,
+        })
+    }
+
     return (
-        <Card className="mx-auto max-w-sm">
-            <CardHeader>
-                <CardTitle className="text-2xl">Login</CardTitle>
-                <CardDescription>
-                    Enter your email below to login to your account
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <div className="grid gap-4">
-                    <div className="grid gap-2">
-                        <Label htmlFor="email">Email</Label>
-                        <Input
-                            id="email"
-                            type="email"
-                            placeholder="m@example.com"
-                            required
-                        />
-                    </div>
-                    <div className="grid gap-2">
-                        <div className="flex items-center">
-                            <Label htmlFor="password">Password</Label>
-                            <Link href="#" className="ml-auto inline-block text-sm underline">
-                                Forgot your password?
-                            </Link>
+        <form onSubmit={submitForm}>
+            <Card className="mx-auto max-w-sm">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                            />
                         </div>
-                        <Input id="password" type="password" required />
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                                <Link href="#" className="ml-auto inline-block text-sm underline">
+                                    Forgot your password?
+                                </Link>
+                            </div>
+                            <Input id="password" type="password" required />
+                        </div>
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Login with Google
+                        </Button>
                     </div>
-                    <Button type="submit" className="w-full">
-                        Login
-                    </Button>
-                    <Button variant="outline" className="w-full">
-                        Login with Google
-                    </Button>
-                </div>
-                <div className="mt-4 text-center text-sm">
-                    Don&apos;t have an account?{""}
-                    <Link href="#" className="underline">
-                        Sign up
-                    </Link>
-                </div>
-            </CardContent>
-        </Card>
+                    <div className="mt-4 text-center text-sm">
+                        Don&apos;t have an account?{""}
+                        <Link href="#" className="underline">
+                            Sign up
+                        </Link>
+                    </div>
+                </CardContent>
+            </Card>
+        </form>
+
     )
 }
